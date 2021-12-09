aefc32437a0ac97b6f4ffbaa55769f987c18a97d
Ethan Lee <33e.lee@gmail.com>
2021-12-09 00:28:18 -0800

    add comments to RawTypes

--- generics/RawTypes.java
+++ generics/RawTypes.java
@@ -1,10 +1,12 @@
 package generics;
 
-class Stack<T> {}
+import java.util.*;
+
 public class RawTypes {
   public static void main(String[] args) {
     // 2 below say Stack should be parameterized
     Stack s1 = new Stack();
+    Stack s6 = new Stack<>();
     Stack s2 = new Stack<Double>();
 
     // below need type safety: since we could add any type to this stack
@@ -12,5 +14,22 @@ public class RawTypes {
 
     Stack<Integer> s4 = new Stack<>(); // Integer is inferred
     Stack<Integer> s5 = new Stack<Integer>();
+
+    // ### Notice how we can push a string and integer to s1
+    s1.push(123);
+    System.out.println(s1.peek());
+    s1.push("String");
+    System.out.println(s1.peek());
+
+    // ### Notice how we can push a string and integer to s2
+    s2.push(123);
+    System.out.println(s2.peek());
+    s2.push("String");
+    System.out.println(s2.peek());
+
+    s4.push(123);
+    System.out.println(s4.peek());
+    // s4.push("String"); // uncomment this, see what happens
+    // System.out.println(s4.peek());
   }
 }
